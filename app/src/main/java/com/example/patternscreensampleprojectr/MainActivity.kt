caca7f0dac70500ae969bf4335f468c218e59379
n22301003-sysong <sysong@pentasecurity.com>
2024-01-09 18:34:55 +0900

    Preview추가

--- app/src/main/java/com/example/patternscreensampleprojectr/MainActivity.kt
+++ app/src/main/java/com/example/patternscreensampleprojectr/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.pattern.BasePatternScreen
 import com.example.pattern.DrawingSetting
@@ -54,4 +57,32 @@ class MainActivity : ComponentActivity() {
 
 private fun ComponentActivity.showToast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
+
+@Preview
+@Composable
+fun PatternPreview() {
+
+    MaterialTheme {
+        Column(
+            modifier = Modifier.background(Color.White)
+        ) {
+            Spacer(modifier = Modifier.weight(1f))
+            BasePatternScreen(
+                modifier = Modifier.size(400.dp),
+                drawingSetting = DrawingSetting(
+                    dotSize = 10.dp,
+                    lineWidth = 4.dp,
+                    lineColor = Color.Cyan,
+                    selectedDotColor = Color.Magenta,
+                    unselectedDotColor = Color.Blue,
+                    minimumLineConnectionCount = 3,
+                ),
+                onLessCountPatternSelected = {  },
+                onPatternSuccessfullySelected = {  }
+            )
+            Spacer(modifier = Modifier.weight(1f))
+        }
+    }
+
 }
