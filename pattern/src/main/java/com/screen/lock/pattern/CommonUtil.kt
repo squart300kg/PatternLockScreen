ac26a9fad64f6e20bd76627d9a69b74172f57439
n22301003-sysong <sysong@pentasecurity.com>
2024-01-10 09:44:17 +0900

    vibrate버전 분기 코드 추가

--- pattern/src/main/java/com/screen/lock/pattern/CommonUtil.kt
+++ pattern/src/main/java/com/screen/lock/pattern/CommonUtil.kt
@@ -9,22 +9,18 @@ import android.os.VibratorManager
 object CommonUtil {
 
     fun vibrate(context: Context, millSecond: Long = 5) {
-        getVibrator(context).vibrate(
-            VibrationEffect.createOneShot(
-                millSecond,
-                VibrationEffect.DEFAULT_AMPLITUDE
-            )
-        )
-    }
-
-    private fun getVibrator(context: Context) =
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            val vibratorManager =
-                context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
-            vibratorManager.defaultVibrator
-        } else {
-            @Suppress("DEPRECATION")
-            context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-        }
 
+            (context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager)
+                .defaultVibrator
+                .vibrate(
+                    VibrationEffect.createOneShot(
+                        millSecond,
+                        VibrationEffect.DEFAULT_AMPLITUDE
+                    )
+                )
+        } else {
+            (context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(millSecond)
+        }
+    }
 }
